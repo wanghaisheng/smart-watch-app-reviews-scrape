cccacfa59a9b323395811a0b7fb3b2ce6ec1bc78
HeisenBerg? <admin@tiktokastudio.com>
2024-06-24 12:36:49 +0800

    Update no.js

--- no.js
+++ no.js
@@ -2,6 +2,14 @@ https://github.com/facundoolano/app-store-scraper
 
 https://github.com/rossmartin/app-store-scraper-reviews
 
+How is this different than getting reviews using app-store-scraper?
+That Node.js module uses a separate API and can get up to 500 reviews maximum. This module allows getting all reviews by using a separate API and technique.
+
+At a high level this is a port of the python app-store-scraper. I implemented that technique and approach here for this Node.js module.
+
+This module does what SerpApi sells but this is free.
+
+  
 var store = require('app-store-scraper'); // regular non caching version
 var memoized = require('app-store-scraper').memoized(); // cache with default options
 var memoizedCustom = require('app-store-scraper').memoized({ maxAge: 1000 * 60 }); // cache with default options
